df7505c40965cc6c0bbd853f7f3b9cd8bd7e74a2
Mike Eshva <reg@eshva.ru>
2021-02-22 18:02:05 +0300

    documentation for IDiContainerAdapter and removed unneeded

--- sources/Eshva.Poezd.Core/Common/IDiContainerAdapter.cs
+++ sources/Eshva.Poezd.Core/Common/IDiContainerAdapter.cs
@@ -1,13 +1,24 @@
 #region Usings
 
 using System;
+using JetBrains.Annotations;
 
 #endregion
 
 namespace Eshva.Poezd.Core.Common
 {
+  /// <summary>
+  /// Poezd adapter for DI-container.
+  /// </summary>
   public interface IDiContainerAdapter : IServiceProvider
   {
+    /// <summary>
+    /// Begins a container scope.
+    /// </summary>
+    /// <returns>
+    /// Disposable object to end gotten scope.
+    /// </returns>
+    [NotNull]
     IDisposable BeginScope();
   }
 }
